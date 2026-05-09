115a7aeafc5323f120902b1a86b997986916ab04
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-05-09 14:51:34 -0300

    feat: adiciona header ao projeto

---
+++ app/views/componentes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <link rel="stylesheet" href="/public/assets/css/global.css">
+  <link rel="stylesheet" href="/public/assets/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header class="p-3  text-white" style="background-color: var(--primary-800,  #59416f);">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <img class="logo-site" src="/public/assets/img/logo-site.png" alt="Logo do Site" class="logo">
+     
+      <ul
+         class="nav col-12 col-lg-auto ms-auto mb-2 justify-content-center mb-md-0"
+      >
+        <li><a href="#" class="nav-link px-3 " style="color: var(--pink-500, #c78ab8); ">Home</a></li>
+        <li><a href="#" class="nav-link px-3 text-white">Serviços</a></li>
+        <li><a href="#" class="nav-link px-3 text-white">Meus Horários</a></li>
+        <li><a href="#" class="nav-link btn-agendar text-white px-4" style="border-bottom: 2px solid #c78ab8;padding-bottom: 6px;font-weight: 600;transition: 0.3s;">Agendar</a></li>
+      </ul>
+    </div>
+  </div>
+</header>
