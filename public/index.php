0f589a368828042a8102487e69d1c57f98c5caa1
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-05-19 19:33:47 -0300

    feat: implementa roteamento de páginas com .htaccess

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 
-require_once '../app/views/index/index.php';
+$page = $_GET['page'] ?? 'index';
+
+switch ($page) {
+
+    case 'agendamento':
+        require_once __DIR__ . '/../app/views/agendamento/agendamento.php';
+        break;
+
+    case 'dashboard':
+        require_once __DIR__ . '/../app/views/dashboard/dashboard.php';
+        break;
+
+    case 'relatorio':
+        require_once __DIR__ . '/../app/views/relatorio/relatorio.php';
+        break;
+
+    case 'seushorarios':
+        require_once __DIR__ . '/../app/views/seushorarios/seushorarios.php';
+        break;
+
+    default:
+        require_once __DIR__ . '/../app/views/index/index.php';
+        break;
+}
