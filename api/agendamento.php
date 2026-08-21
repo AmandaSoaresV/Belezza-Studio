2a8768f50a828c7f67e3d89ea2fa30d1295d2c76
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-08-21 20:06:50 -0300

    fix(api): adiciona headers CORS e tratamento de erros

--- api/agendamento.php
+++ api/agendamento.php
@@ -1,11 +1,22 @@
 <?php
     header('Content-Type: application/json, charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET");
+
     require_once __DIR__ . '/conexao.php';
 
+try {
     $sql = "SELECT * FROM agendamentos ORDER BY id_agendamento DESC";
     $consultaAgendamentos = $pdo->prepare($sql);
     $consultaAgendamentos->execute();
 
     $dadosAgendamentos = $consultaAgendamentos->fetchAll(PDO::FETCH_ASSOC);
+
     echo json_encode($dadosAgendamentos);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode([
+        "error" => "Erro ao executar consulta no banco de dados"
+    ]);
+}
 ?>
--- api/servico.php
+++ api/servico.php
@@ -1,6 +1,10 @@
 <?php
     header('Content-Type: application/json, charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET");
     require_once __DIR__ . '/conexao.php';
+
+    try{
 
     $sql = "SELECT * FROM servicos ORDER BY id_servico DESC";
     $consultaServicos = $pdo->prepare($sql);
@@ -8,4 +12,9 @@
 
     $dadosServicos = $consultaServicos->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($dadosServicos);
+    }
+    catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(["error" => "Erro ao executar consulta no banco de dados"]);
+    }
 ?>
