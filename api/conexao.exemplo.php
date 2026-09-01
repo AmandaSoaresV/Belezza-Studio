ecbafdb427fb21b77f4acc0befe9d3a47f380a9e
amandasoaresv <amandasoaresvieira5@gmail.com>
2026-09-01 10:19:26 -0300

    chore(projeto): corrige porta do exemplo de conexao e remove pastas vazias

--- api/conexao.exemplo.php
+++ api/conexao.exemplo.php
@@ -6,7 +6,7 @@ $host = "localhost";
 $db   = "seu_banco";
 $user = "seu_usuario";
 $pass = "sua_senha";
-$port = 3306;
+$port = 3307;
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
